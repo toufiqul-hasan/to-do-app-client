a9db16283dbfc1c2e6017da090feb4f7c80c9d38
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-05-30 08:56:46 +0600

    footer added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer mt-auto">
+      <h2>To Do App</h2>
+      <p>Copyright &copy; 2022 To Do App</p>
+      <p>All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
